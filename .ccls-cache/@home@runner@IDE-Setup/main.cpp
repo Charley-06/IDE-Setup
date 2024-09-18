8df8a3564b69c42bb489e0f9ec6660bccf2f0c1d
Charley <bobosoepic@gmail.com>
2024-09-18 03:44:04 +0000

    More changes to checkpoint 17

--- .ccls-cache/@home@runner@IDE-Setup/main.cpp
+++ .ccls-cache/@home@runner@IDE-Setup/main.cpp
@@ -1,12 +1,13 @@
 // This programe will display my name, address, and phone number in three separate string objects.
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() 
 { 
-  char name, adress, number;
-  name = "Charley Huber";
-  address = "2241 New Kent Ct.";
+  string name, address, number;
+  name = "Charley Huber\n";
+  address = "2241 New Kent Ct.\n";
   number = "843 598 0966";
   cout << name << address << number << endl;
   return 0;

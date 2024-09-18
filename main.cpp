f82d65922a78c403723eacaf9b66048606485605
Charley <bobosoepic@gmail.com>
2024-09-18 03:21:15 +0000

    Cleared out main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
-// Display my initials
+
 #include <iostream>
 using namespace std;
 
 int main() 
 { 
-  char first, middle, last;
-  first = 'C';
-  middle = 'W';
-  last = 'H';
-  cout << first << middle << last << endl;
-  return 0;
+
 }

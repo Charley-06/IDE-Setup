5dda25f7c437d771ed978ea3dff1fe5a075fca84
Charley <bobosoepic@gmail.com>
2024-09-18 03:49:39 +0000

    Made changes to 2.20

--- .ccls-cache/@home@runner@IDE-Setup/main.cpp
+++ .ccls-cache/@home@runner@IDE-Setup/main.cpp
@@ -1,8 +1,11 @@
-
+// Displays age and weight
 #include <iostream>
 using namespace std;
 
 int main() 
 { 
-
+  string age, weight;
+  age = "18\n";
+  weight = "20\n";
+  cout << "My age is " << age << "and my weight is " << weight;
 }
